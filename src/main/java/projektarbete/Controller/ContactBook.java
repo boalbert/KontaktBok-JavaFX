ddded97eb19ec19670243b7b1dfbda1ec66e1020
JannisMueller <jannis.mueller@hotmail.de>
2020-11-27 10:38:58 +0100

    add ULM

---
+++ src/main/java/projektarbete/Controller/ContactBook.java
@@ -0,0 +1,13 @@
+package projektarbete.Controller;
+
+
+import projektarbete.Model.WorkContact;
+
+import java.util.HashMap;
+
+public class ContactBook {
+
+    public static HashMap<String, WorkContact> workContactHashMap = new HashMap<>();//load json-file
+
+
+}
--- src/main/java/projektarbete/Main/Main.java
+++ src/main/java/projektarbete/Main/Main.java
@@ -1,4 +1,11 @@
 package projektarbete.Main;
 
+import projektarbete.Controller.ContactBook;
+import projektarbete.Model.Contact;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
 }
