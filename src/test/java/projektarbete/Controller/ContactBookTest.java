2c77fcf29ff834a74865d03b631e6b013c790499
JannisMueller <jannis.mueller@hotmail.de>
2020-11-27 15:50:31 +0100

    adding unitstest to class WorkContacts

---
+++ src/test/java/projektarbete/Controller/ContactBookTest.java
@@ -0,0 +1,17 @@
+package projektarbete.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactBookTest {
+
+    @Test
+    void printContactBook() {
+
+    }
+
+    @Test
+    void addWorkContactToContactBook() {
+    }
+}
