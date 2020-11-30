ef0c83e9d707cb96f8041d6ceca5be6719b7242e
Chokealot <70509435+Chokealot@users.noreply.github.com>
2020-11-30 13:23:08 +0100

    --Added testClasses for ContactBookTest.java

---
+++ src/test/java/projektarbete/Controller/ContactBookTest.java
@@ -0,0 +1,33 @@
+package projektarbete.Controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContactBookTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void searchContact() {
+    }
+
+    @Test
+    void removeContact() {
+    }
+
+    @Test
+    void updateContactBook() {
+    }
+
+    @Test
+    void printContactBook() {
+    }
+
+    @Test
+    void addWorkContactToContactBook() {
+    }
+}
