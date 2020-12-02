52b219857712dae2b9475af41deb1133436ac469
boalbert <andersson.albert@gmail.com>
2020-12-02 08:46:07 +0100

    Created empty ioHandling.test

---
+++ src/test/java/projektarbete/data/ioHandlingTest.java
@@ -0,0 +1,16 @@
+package projektarbete.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ioHandlingTest {
+
+    @Test
+    void loadHashMapFromJson() {
+    }
+
+    @Test
+    void saveHashMapToJson() {
+    }
+}
